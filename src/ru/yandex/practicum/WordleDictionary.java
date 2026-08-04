03b85c8b33ed8697c33267d25194b55c0bf4b63b
Ksenia Rusinova <krusinova@mtsbank.ru>
2026-08-04 11:04:39 +0300

    обработка файла

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,6 +1,10 @@
 package ru.yandex.practicum;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+
+import ru.yandex.practicum.WordleDictionaryLoader.*;
 
 /*
 этот класс содержит в себе список слов List<String>
@@ -9,6 +13,36 @@ import java.util.List;
  */
 public class WordleDictionary {
 
-    private List<String> words;
+    private List<String> words = new ArrayList<>();
 
+    WordleDictionaryLoader wordleDictionaryLoader = new WordleDictionaryLoader();
+
+    public void filterListByLength() throws IOException {
+        List<String> internalList = wordleDictionaryLoader.readWordsFromFile("words_ru.txt");
+
+        for(String word : internalList){
+            if(word.length() == 5){
+                words.add(normalizeYoToEe(word).toLowerCase());
+            }
+        }
+    }
+
+    private String normalizeYoToEe(String word) {
+        if (word == null) return null;
+
+        StringBuilder sb = new StringBuilder(word);
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (c == 'ё') {
+                sb.setCharAt(i, 'е');
+            } else if (c == 'Ё') {
+                sb.setCharAt(i, 'Е');
+            }
+        }
+        return sb.toString();
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
 }
